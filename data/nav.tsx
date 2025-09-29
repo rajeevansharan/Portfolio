45159f4fc91f20c518988bcb027ec52f383f19c1
rajeevansharan <rajeevansharan@gmail.com>
2025-09-29 23:46:21 +0530

    feat: add Card component and navigation items for the portfolio

---
+++ data/nav.tsx
@@ -0,0 +1,8 @@
+
+import React from "react";
+
+export const navItems = [
+  { name: "About", link: "#about" },
+  { name: "Projects", link: "#projects" },
+  { name: "Contact", link: "#contact" },
+];
